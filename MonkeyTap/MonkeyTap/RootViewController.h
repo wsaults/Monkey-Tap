925499c5c3bb2b98341c5135eea0016bd610fb6f
William Saults <wsaults@email.itt-tech.edu>
2012-04-18 12:29:08 -0500

    Created the xcode project

---
+++ MonkeyTap/MonkeyTap/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  MonkeyTap
+//
+//  Created by William Saults on 4/18/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
